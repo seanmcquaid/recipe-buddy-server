ab0867caf9cd3b302f2fbe0d70a5206c65b8c064
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-23 22:26:24 -0400

    finished setting up testing

--- .mocharc.js
+++ .mocharc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  spec: './___tests___/**/*.spec.js',
+  // spec: '___tests___/**/*.js',
 };
--- src/___tests___/controllers/usersController.spec.js
+++ src/___tests___/controllers/usersController.spec.js
@@ -1,1 +1,7 @@
-describe('Name of the group', () => {});
+const expect = require('chai').expect;
+
+describe('Name of the group', () => {
+  it('', () => {
+    expect(200).to.equal(200);
+  });
+});
