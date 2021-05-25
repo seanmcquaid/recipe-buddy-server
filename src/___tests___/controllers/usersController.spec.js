65186e6c225c4634f566c1c297479153059c113f
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-25 19:39:16 -0400

    finished post login tests for controller

--- src/___tests___/controllers/usersController.spec.js
+++ src/___tests___/controllers/usersController.spec.js
@@ -2,12 +2,78 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const UserDao = require('../../daos/UserDao');
 const usersController = require('../../controllers/usersController');
+const { hashPassword } = require('../../utils/passwordUtils');
+const mockRequest = require('../../testUtils/mockRequest');
+const mockResponse = require('../../testUtils/mockResponse');
 
 describe('usersController', () => {
   describe('postLogin', () => {
-    sinon.stub(UserDao, 'findOne').withArgs({ username: 'sean' }).returns(null);
-    usersController.postLogin({
-      body: { username: 'sean', password: 'hello' },
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it("User doesn't exist", async () => {
+      sinon
+        .stub(UserDao, 'findOne')
+        .withArgs({ username: 'sean' })
+        .returns(null);
+
+      const req = mockRequest({}, { username: 'sean', password: 'hello' });
+      const res = mockResponse();
+
+      await usersController.postLogin(req, res);
+
+      expect(res.status.calledWith(401)).to.be.true;
+      expect(
+        res.json.calledWith({
+          errorMessage: "This user doesn't exist, please try again!",
+        }),
+      ).to.be.true;
+    });
+
+    it('Password is incorrect', async () => {
+      sinon
+        .stub(UserDao, 'findOne')
+        .withArgs({ username: 'sean' })
+        .returns({
+          username: 'sean',
+          password: hashPassword('hello'),
+        });
+
+      const req = mockRequest(
+        {},
+        { username: 'sean', password: 'hello there' },
+      );
+      const res = mockResponse();
+
+      await usersController.postLogin(req, res);
+
+      expect(res.status.calledWith(401)).to.be.true;
+      expect(
+        res.json.calledWith({
+          errorMessage: "This password isn't correct, please try again!",
+        }),
+      ).to.be.true;
+    });
+
+    it('Valid user info', async () => {
+      sinon
+        .stub(UserDao, 'findOne')
+        .withArgs({ username: 'sean' })
+        .returns({
+          username: 'sean',
+          password: hashPassword('hello'),
+        });
+
+      const req = mockRequest({}, { username: 'sean', password: 'hello' });
+      const res = mockResponse();
+
+      await usersController.postLogin(req, res);
+
+      expect(res.status.calledWith(200)).to.be.true;
+      expect(res.json.args[0][0].token.length).greaterThan(0);
     });
   });
+
+  describe('postRegister', () => {});
 });
--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -2,7 +2,10 @@ const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 exports.hashPassword = (password) =>
-  bcrypt.hashSync(password, process.env.BCRYPT_SALT_ROUNDS);
+  bcrypt.hashSync(
+    password,
+    Number.parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10,
+  );
 
 exports.verifyPassword = (password, hashedPassword) =>
   bcrypt.compareSync(password, hashedPassword);
