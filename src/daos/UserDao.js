f3aa9b5446d38edd740c4af637243481ede1a89d
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-12 12:23:07 -0400

    added methods for add recipe and delete recipe

--- src/daos/UserDao.js
+++ src/daos/UserDao.js
@@ -12,6 +12,16 @@ const userSchema = new Schema({
   ],
 });
 
+userSchema.methods.addRecipe = function (recipe) {
+  this.recipes.push(recipe);
+  return this.save();
+};
+
+userSchema.methods.deleteRecipe = function (recipeId) {
+  this.recipes = this.recipes.filter((recipe) => recipe.id !== recipeId);
+  return this.save();
+};
+
 const UserDao = mongoose.model('User', userSchema);
 
 module.exports = UserDao;
