129a302418ef772d9f557929bae41ed822263b6f
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-12 12:25:42 -0400

    finished post add controller

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -2,9 +2,29 @@ const UserDao = require('../daos/UserDao');
 const { decodeJwt } = require('../utils/jwtUtils');
 
 exports.getRecipes = async (req, res) => {
-  const { username } = decodeJwt(req.token);
   try {
+    const { username } = decodeJwt(req.token);
     const { recipes } = await UserDao.findOne({ username });
+    return res.status(200).json({
+      recipes,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: 'There was an issue with getting recipes',
+    });
+  }
+};
+
+exports.postAddRecipe = async (req, res) => {
+  try {
+    const { username } = decodeJwt(req.token);
+    const { id, title } = req.body;
+    const userInfo = await UserDao.findOne({ username });
+
+    await userInfo.addRecipe({ id, title });
+
+    const { recipes } = await UserDao.findOne({ username });
+
     return res.status(200).json({
       recipes,
     });
