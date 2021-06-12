e9a99fc250c0a47d75287c53c84449ce23c17d76
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-12 12:28:24 -0400

    finished delete recipe endpoint

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -34,3 +34,23 @@ exports.postAddRecipe = async (req, res) => {
     });
   }
 };
+
+exports.deleteRecipe = async (req, res) => {
+  try {
+    const { username } = decodeJwt(req.token);
+    const { id } = req.params;
+    const userInfo = await UserDao.findOne({ username });
+
+    await userInfo.deleteRecipe(id);
+
+    const { recipes } = await UserDao.findOne({ username });
+
+    return res.status(200).json({
+      recipes,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: 'There was an issue with getting recipes',
+    });
+  }
+};
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,4 +5,8 @@ const recipesController = require('../controllers/recipesController');
 
 recipesRouter.get('/', checkAuth, recipesController.getRecipes);
 
+recipesRouter.post('/', checkAuth, recipesController.postAddRecipe);
+
+recipesRouter.delete('/:id', checkAuth, recipesController.deleteRecipe);
+
 module.exports = recipesRouter;
