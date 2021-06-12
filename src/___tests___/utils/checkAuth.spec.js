12c90ec36680697509b50828baf05dffbeb0c9ff
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-12 13:03:44 -0400

    finished check auth tests

--- src/___tests___/utils/checkAuth.spec.js
+++ src/___tests___/utils/checkAuth.spec.js
@@ -3,12 +3,13 @@ const mockRequest = require('../../testUtils/mockRequest');
 const mockResponse = require('../../testUtils/mockResponse');
 const mockNext = require('../../testUtils/mockNext');
 const expect = require('chai').expect;
+const { createJwt } = require('../../utils/jwtUtils');
 
 describe('checkAuth', () => {
   it('No token provided', () => {
     const req = mockRequest({});
     const res = mockResponse();
-    const next = mockNext();
+    const next = mockNext;
 
     checkAuth(req, res, next);
 
@@ -21,14 +22,33 @@ describe('checkAuth', () => {
   });
 
   it('Valid token provided', () => {
-    const req = mockRequest();
+    const req = mockRequest(
+      { Authorization: createJwt(1, 'sean') },
+      {},
+      {},
+      {},
+    );
     const res = mockResponse();
-    const next = mockNext();
+    const next = mockNext;
+
+    checkAuth(req, res, next);
+
+    expect(req.token).to.not.be.null;
+    expect(next.callCount).equal(1);
   });
 
   it('Invalid token provided', () => {
-    const req = mockRequest();
+    const req = mockRequest({ Authorization: 'Bad token' }, {}, {}, {});
     const res = mockResponse();
-    const next = mockNext();
+    const next = mockNext;
+
+    checkAuth(req, res, next);
+
+    expect(res.status.calledWith(401)).to.be.true;
+    expect(
+      res.json.calledWith({
+        errorMessage: 'Expired Token',
+      }),
+    ).to.be.true;
   });
 });
